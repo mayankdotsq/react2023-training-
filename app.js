583bf196bb69564eaa5ba756f29135c18bca5040
mayankdotsq <mayank.mourya@dotsquares.com>
2023-07-13 00:28:50 +0530

    Changes in the Package.json fine and created functional comp

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
+Button=()=>{
+    return(
+        <>
+       <input type="button" defaultValue="Click here for Sign-up" onclick="msg()" />
+        </>
+    )
+}
+
 App=()=>{
    return(<>
-    <h1>
+   <p>
+   <h1>
         Welcome to my app
     </h1>
+   </p>
+   <Button/>
     </>) 
 }
 
